d5cb0ac1d915237c030da534d4602374c94bcab3
Tom <tluck@Toms-MBP16.local>
2020-07-05 23:06:37 -0700

    2020.07.05 Update
    reduce memory  footprint

--- k8s/deploy_ProdDB.bash
+++ k8s/deploy_ProdDB.bash
@@ -52,7 +52,8 @@ do
     kubectl get mongodb/my-replica-set
     eval status=$( kubectl get mongodb/my-replica-set -o json| jq '.status.phase' )
     kubectl get mongodb/my-replica-set -o json| jq '.status.message' 
-    if [[ $status == "Pending" || $status == "Running" ]];
+    #if [[ $status == "Pending" || $status == "Running" ]];
+    if [[ $status == "Running" ]];
     then
         printf "%s\n" "$status"
         break

87e7df4e412c9531b280a259f0510f1128daa006
Tom <tluck@Toms-MBP16.local>
2020-07-07 23:04:14 -0700

    2020-07-07 Updates
    add LoadBalancer option for OPSMGR

--- k8s/Misc/connect_to_my-replica-set.bash
+++ k8s/Misc/connect_to_my-replica-set.bash
@@ -1,1 +1,2 @@
-kubectl exec my-replica-set-0 -i -t -- /var/lib/mongodb-mms-automation/bin/mongo mongodb://my-replica-set-0:27017/?replicaSet=my-replica-set
+#kubectl exec my-replica-set-0 -i -t -- /var/lib/mongodb-mms-automation/bin/mongo mongodb://my-replica-set-0:27017/?replicaSet=my-replica-set
+kubectl exec my-replica-set-0 -i -t -- /var/lib/mongodb-mms-automation/bin/mongo mongodb://my-replica-set-0:27017/?replicaSet=my-replica-set -ssl --sslCAFile /mongodb-automation/ca.pem --sslPEMKeyFile /mongodb-automation/server.pem -u dbAdmin -p Mongodb1$ 
--- k8s/deploy_OM.bash
+++ k8s/deploy_OM.bash
@@ -5,13 +5,13 @@
 # Create the metrics server
 kubectl apply -f /opt/Source/metrics-server/components.yaml 
 
-# Create the namespace and MongoDB Enterprise Operator
+# Create the namespace and context
 kubectl create namespace mongodb
 kubectl config set-context $(kubectl config current-context) --namespace=mongodb
+
+# Deploy and MongoDB Enterprise Operator
 kubectl apply -f crds.yaml
 kubectl apply -f mongodb-enterprise.yaml
-#kubectl get all -n mongodb
-#kubectl describe pods -n mongodb
 
 # Create the credentials for main admin user
 kubectl delete secret admin-user-credentials > /dev/null 2>&1
@@ -22,7 +22,8 @@ kubectl create secret generic admin-user-credentials \
   --from-literal=LastName="${lastName}"
 
 #  Deploy OpsManager resources
-kubectl apply -f ops-mgr-resource.yaml
+## kubectl apply -f ops-mgr-resource-ext-np.yaml
+kubectl apply -f ops-mgr-resource-ext-lb.yaml
 
 # Monitor the progress until the OpsMgr app is ready
 while true
@@ -43,3 +44,4 @@ cat init.conf |sed -e '/opsMgrUrl/d' -e '/opsMgrIp/d' > new
 echo  opsMgrUrl="$opsMgrUrl" | tee -a new
 echo  opsMgrIp="$opsMgrIp"  | tee -a new
 mv new init.conf
+kubectl get svc/opsmanager-svc-ext

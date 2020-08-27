873821809a69c98aa2015553b267e794da358e34
Tom <tluck@Toms-MBP16.local>
2020-08-27 16:24:37 -0700

    2020.08.27
    Fixes for connnect string

--- k8s/Misc/connect_external.bash
+++ k8s/Misc/connect_external.bash
@@ -6,14 +6,15 @@ name=${1:-my-replica-set}
 # get keys for TLS
 kubectl exec ${name}-0 -i -t -- cat /mongodb-automation/ca.pem > ca.pem
 kubectl exec ${name}-0 -i -t -- cat /mongodb-automation/server.pem > server.pem
-
-if [[ "$( kubectl get mdb/${name} -o jsonpath='{.spec.security.tls}' )" == "map[enabled:true]" ]]
+tls=$( kubectl get mdb/${name} -o jsonpath='{.spec.security.tls}' )
+if [[ "${tls}" == "map[enabled:true]" || "${tls}" == "{\"enabled\":true}" ]]
 then
     #tls_options="--tls --tlsCAFile ca.pem --tlsCertificateKeyFile server.pem "
-    tls_options="&tls=true&tlsCAFile=ca.pem&tlsCertificateKeyFile=server.pem"
+    #tls_options="&tls=true&tlsCAFile=ca.pem&tlsCertificateKeyFile=server.pem"
+    tls_options="&tls=true"
 fi
 
 eval cs=\$${name//-/}_URI
-printf "\n%s\n\n" "Connect String: ${cs}${tls_options}"
 fcs=\'${cs}${tls_options}\'
+printf "\n%s\n\n" "Connect String: ${fcs}"
 eval mongo "${fcs}"
--- k8s/Misc/kub_connect_to_pod.bash
+++ k8s/Misc/kub_connect_to_pod.bash
@@ -3,12 +3,12 @@
 source init.conf
 name=${1:-my-replica-set}
 
-if [[ "$( kubectl get mdb/${name} -o jsonpath='{.spec.security.tls}' )" == 'map[enabled:true]' ]]
+tls=$( kubectl get mdb/${name} -o jsonpath='{.spec.security.tls}' )
+if [[ "${tls}" == "map[enabled:true]" || "${tls}" == "{\"enabled\":true}" ]]
 then
     #tls_options="&tls=true&tlsCAFile=/mongodb-automation/ca.pem&tlsCertificateKeyFile=/mongodb-automation/server.pem"
     tls_options=" --tls --tlsCAFile /mongodb-automation/ca.pem --tlsCertificateKeyFile /mongodb-automation/server.pem"
 fi
 
-#string="kubectl exec ${name}-0 -i -t -- /var/lib/mongodb-mms-automation/bin/mongo mongodb://${name}-0:27017/?replicaSet=${name} -u $dbadmin -p $dbpassword --authenticationDatabase admin $tls_options"
-string="kubectl exec ${name}-0 -i -t -- /var/lib/mongodb-mms-automation/bin/mongo mongodb://${dbadmin}:${dbpassword}@${name}-0:27017/admin?replicaSet=${name} $tls_options"
+string="kubectl exec ${name}-0 -i -t -- /var/lib/mongodb-mms-automation/bin/mongo mongodb://${dbadmin}:${dbpassword}@${name}-0:27017/?replicaSet=${name} $tls_options"
 eval $string
